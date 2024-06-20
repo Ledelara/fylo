cd1c10df0a5019ffd6afae4f0bcad3d82c834b1f
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-06-20 17:28:24 -0300

    :recycle: Refactor: chamando FunctionalitiesComponentComponent

--- src/app/modules/pages/main-page/main-page.component.ts
+++ src/app/modules/pages/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponentComponent } from '../../components/header-component/header-component.component';
 import { IntroComponentComponent } from '../../components/intro-component/intro-component.component';
+import { FunctionalitiesComponentComponent } from '../../components/functionalities-component/functionalities-component.component';
 
 @Component({
   selector: 'app-main-page',
@@ -8,6 +9,7 @@ import { IntroComponentComponent } from '../../components/intro-component/intro-
   imports: [
     HeaderComponentComponent,
     IntroComponentComponent,
+    FunctionalitiesComponentComponent,
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
