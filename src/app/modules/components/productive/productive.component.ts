a44886b74579fd1c0ff04a6e8fd78d12b1303f06
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-06-21 11:29:02 -0300

    :sparkles: Feat: ProductiveComponent

---
+++ src/app/modules/components/productive/productive.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productive',
+  standalone: true,
+  imports: [],
+  templateUrl: './productive.component.html',
+  styleUrl: './productive.component.scss'
+})
+export class ProductiveComponent {
+
+}
